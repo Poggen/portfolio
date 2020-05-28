f992209f93a3252708dcd1ad261e4a0325221f01
Jakob Magnusson <j@jakes.se>
2020-05-28 16:06:58 +0200

    Updated contact

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -5,7 +5,7 @@ import styles from "./footer.module.css"
 const Footer = ({ title, date, url }) => (
   <footer className={styles.footer} >
     <p>This site does not track you or collect analytics.</p>
-    <p>Built with <a href="https://www.gatsbyjs.org/">Gatsby</a> and hosted on <a href="https://www.netlify.com/">Netlify</a></p>
+    <p>Built with <a href="https://www.gatsbyjs.org/">Gatsby</a> and hosted on <a href="https://www.netlify.com/">Netlify.</a></p>
   </footer>
 )
 
