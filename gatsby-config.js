8715eb29cc96e69cb5c1b6efe4919add76b2cfa3
Jakob Magnusson <j@jakes.se>
2021-08-15 01:24:46 +0200

    Update

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,25 +67,6 @@ module.exports = {
         ],
       },
     },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        // edit below
-        // trackingId: `ADD YOUR TRACKING ID HERE`,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        // edit below
-      },
-    },
   ],
 }
 
