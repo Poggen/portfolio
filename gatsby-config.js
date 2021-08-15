957f6b00ee8fe78421a524e604b50ca1cc329854
Jakob Magnusson <j@jakes.se>
2021-08-15 17:44:03 +0200

    Added react-is

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,7 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
+    `react-is`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
