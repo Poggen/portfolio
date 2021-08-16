75fc78f5d0dabcf5887c4661788f0b8ae3a9b7af
Jakob Magnusson <j@jakes.se>
2021-08-16 13:51:18 +0200

    Removed gatsby-remark-copy-linked-files

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,9 +59,6 @@ module.exports = {
             resolve: `gatsby-remark-vscode`,
           },
           {
-            resolve: `gatsby-remark-copy-linked-files`,
-          },
-          {
             resolve: `gatsby-remark-smartypants`,
           },
         ],
