6576993f841b26da1482f3bf2f9584c31d9530d1
Jakob Magnusson <j@jakes.se>
2021-08-16 14:01:52 +0200

    Removed gatsby-remark-images

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,10 +44,6 @@ module.exports = {
         extensions: [".mdx", ".md"],
         gatsbyRemarkPlugins: [
           {
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 590,
-            },
           },
           {
             resolve: `gatsby-remark-responsive-iframe`,
