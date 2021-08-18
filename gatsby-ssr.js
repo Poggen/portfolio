69771d098dabcbfbbd0c55dd286f47b3e64b3bc1
jakobmagnus <53524961+jakobmagnus@users.noreply.github.com>
2021-08-18 12:00:23 +0200

    Delete gatsby-ssr.js

--- gatsby-ssr.js
+++
@@ -1,8 +0,0 @@
-const React = require("react")
-const Layout = require("./src/components/layout")
-
-exports.onRenderBody = ({ setBodyAttributes }) => {
-    setBodyAttributes({
-        className: "body-bg",
-    })
-}
