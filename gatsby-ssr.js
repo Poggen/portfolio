633dff72e013ef1226a8858b278d26bc2bdebc23
Jakob Magnusson <j@jakes.se>
2021-08-24 10:32:24 +0200

    Update gatsby-ssr.js

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -2,7 +2,7 @@ exports.onRenderBody = ({setBodyAttributes,}) => {
     // Affect the HTML that gets loaded before React here
     setBodyAttributes({
       style: {
-        backgroundColor: 'red',
+        backgroundColor: 'green',
       },
     });
   }
