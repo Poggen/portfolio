b052f7aaaf0e696185a5113a42ed7534cb983fb6
Jakob Magnusson <j@jakes.se>
2022-02-23 17:46:27 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,7 +26,6 @@ class IndexPage extends React.Component {
           My specialities are User Interface Design, Interaction Design and Design Systems.
           I'm passionate about accessibility and late night coding.
         </TextPrimary>
-        {/* 
         <HeaderSecondary title="Case studies" />
         <TextPrimary>
           {caseStudiesPosts.map(({ node }, index) => {
@@ -37,7 +36,6 @@ class IndexPage extends React.Component {
             )
           })}
         </TextPrimary>
-        */}  
         <HeaderSecondary title="Blog" />
         <TextPrimary>
           {blogPosts.map(({ node }, index) => {
