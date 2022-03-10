5858df2bdbd973e0432c9baf2026d56feb2aa60d
Jakob Magnusson <j@jakes.se>
2022-03-10 20:31:34 +0100

    Added back module

--- src/components/blog/BlogContent.js
+++ src/components/blog/BlogContent.js
@@ -2,10 +2,12 @@ import React from "react"
 import * as styles from "./blog.module.css"
 import { MDXRenderer } from "gatsby-plugin-mdx"
 
-const Blog = ({ children }) => (
-  <div className={styles.content}>
-    <MDXRenderer>{children}</MDXRenderer>
-  </div>
-)
+function Blog({ children }) {
+  return (
+    <div className={styles.content}>
+      <MDXRenderer>{children}</MDXRenderer>
+    </div>
+  )
+}
 
 export default Blog
